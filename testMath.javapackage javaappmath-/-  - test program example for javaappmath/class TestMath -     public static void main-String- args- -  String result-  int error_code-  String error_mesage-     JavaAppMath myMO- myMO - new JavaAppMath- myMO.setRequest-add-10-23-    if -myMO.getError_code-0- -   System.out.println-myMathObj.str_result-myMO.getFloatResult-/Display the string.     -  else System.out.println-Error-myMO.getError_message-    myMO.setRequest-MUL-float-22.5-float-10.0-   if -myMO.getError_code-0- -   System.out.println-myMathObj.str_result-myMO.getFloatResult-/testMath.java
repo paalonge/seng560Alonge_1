798fa767cde515b6a4bd79d8c9e59449d4485fe8
paalonge <31939250+paalonge@users.noreply.github.com>
2017-09-19 23:26:05 -0400

    Update testMath.java

--- testMath.javapackage javaappmath-/-  - test program example for javaappmath/class TestMath -     public static void main-String- args- -  String result-  int error_code-  String error_mesage-     JavaAppMath myMO- myMO - new JavaAppMath- myMO.setRequest-add-10-23-    if -myMO.getError_code-0- -   System.out.println-myMathObj.str_result-myMO.getFloatResult-/Display the string.     -  else System.out.println-Error-myMO.getError_message-    myMO.setRequest-MUL-float-22.5-float-10.0-   if -myMO.getError_code-0- -   System.out.println-myMathObj.str_result-myMO.getFloatResult-/testMath.java
+++ testMath.javapackage javaappmath-/-  - test program example for javaappmath/class TestMath -     public static void main-String- args- -  String result-  int error_code-  String error_mesage-     JavaAppMath myMO- myMO - new JavaAppMath- myMO.setRequest-add-10-23-    if -myMO.getError_code-0- -   System.out.println-myMathObj.str_result-myMO.getFloatResult-/Display the string.     -  else System.out.println-Error-myMO.getError_message-    myMO.setRequest-MUL-float-22.5-float-10.0-   if -myMO.getError_code-0- -   System.out.println-myMathObj.str_result-myMO.getFloatResult-/testMath.java
@@ -17,14 +17,14 @@ myMO.setRequest("add",10,23);
  
 //test add two integer numbers
  if (myMO.getError_code()==0) {
-  System.out.println("My Integer result="+myMO.getIntResult()); 
+  System.out.println("My Addition Integer result="+myMO.getIntResult()); 
     }
  else System.out.println("Error"+myMO.getError_message());
  
  // test multiple two floating numbers
  myMO.setRequest("MUL",(float)22.5,(float)10.0);
   if (myMO.getError_code()==0) {
-  System.out.println("My Float result="+myMO.getFloatResult());  
+  System.out.println("My Muliplication Float result="+myMO.getFloatResult());  
     }
  else System.out.println("Error"+myMO.getError_message());
  
@@ -35,8 +35,39 @@ myMO.setRequest("add",10,23);
     }
  else System.out.println("Error"+myMO.getError_message());
 
+       // test square root
+ myMO.setRequest("sqr",12);
+  if (myMO.getError_code()==0) {
+  System.out.println("My square root Float result="+myMO.getFloatResult());  
+    }
+ else System.out.println("Error"+myMO.getError_message());
+  
+  // test division
+ myMO.setRequest("dIv",100,28);
+  if (myMO.getError_code()==0) {
+  System.out.println("My division Float result="+myMO.getFloatResult()+"Same set My division Int result="+myMO.getIntResult());
+    }
+ else System.out.println("Error"+myMO.getError_message());
+  
+  // test division with divide by zero error
+ myMO.setRequest("dIv",100,0);
+  if (myMO.getError_code()==0) {
+  System.out.println("My division Float result="+myMO.getFloatResult()+"Same set My division Int result="+myMO.getIntResult());
+    }
+ else System.out.println("Error"+myMO.getError_message());
+  
+    // test conversion types
+ myMO.setRequest("add",200,12);
+  if (myMO.getError_code()==0) {
+  System.out.println("My add Float result="+myMO.getFloatResult()
+  +"\n Int="+myMO.getIntResult()
+  +"\n Oct="+myMO.getOctResult()
+  +"\n Hex="+myMO.getHexResult()
+  +"\n Bin="+myMO.getBinResult()
+  );
+    }
+ else System.out.println("Error"+myMO.getError_message());
 }
-
 
 }
         
