431f614af70f6ddcbc46dd50b0bb288df6f5da16
paalonge <31939250+paalonge@users.noreply.github.com>
2017-09-19 22:54:00 -0400

    Update testMath.java

--- testMath.javapackage javaappmath-/-  - test program example for javaappmath/class TestMath -     public static void main-String- args- -  String result-  int error_code-  String error_mesage-     JavaAppMath myMO- myMO - new JavaAppMath- myMO.setRequest-add-10-23-    if -myMO.getError_code-0- -   System.out.println-myMathObj.str_result-myMO.getFloatResult-/Display the string.     -  else System.out.println-Error-myMO.getError_message-    myMO.setRequest-MUL-float-22.5-float-10.0-   if -myMO.getError_code-0- -   System.out.println-myMathObj.str_result-myMO.getFloatResult-/testMath.java
+++ testMath.javapackage javaappmath-/-  - test program example for javaappmath/class TestMath -     public static void main-String- args- -  String result-  int error_code-  String error_mesage-     JavaAppMath myMO- myMO - new JavaAppMath- myMO.setRequest-add-10-23-    if -myMO.getError_code-0- -   System.out.println-myMathObj.str_result-myMO.getFloatResult-/Display the string.     -  else System.out.println-Error-myMO.getError_message-    myMO.setRequest-MUL-float-22.5-float-10.0-   if -myMO.getError_code-0- -   System.out.println-myMathObj.str_result-myMO.getFloatResult-/testMath.java
@@ -9,27 +9,32 @@ package javaappmath;
 
     
 class TestMath {
-    public static void main(String[] args) {
- String result;
- int error_code;
- String error_mesage;
+ public static void main(String[] args) {
 
- 
- 
 JavaAppMath myMO;
 myMO = new JavaAppMath();
 myMO.setRequest("add",10,23);
  
+//test add two integer numbers
  if (myMO.getError_code()==0) {
-  System.out.println("myMathObj.str_result="+myMO.getFloatResult()); // Display the string.
+  System.out.println("My Integer result="+myMO.getIntResult()); 
     }
  else System.out.println("Error"+myMO.getError_message());
  
+ // test multiple two floating numbers
  myMO.setRequest("MUL",(float)22.5,(float)10.0);
   if (myMO.getError_code()==0) {
-  System.out.println("myMathObj.str_result="+myMO.getFloatResult()); // Display the string.
+  System.out.println("My Float result="+myMO.getFloatResult());  
     }
  else System.out.println("Error"+myMO.getError_message());
+ 
+     // test error with bad operator request
+ myMO.setRequest("BAD",(float)22.5,(float)10.0);
+  if (myMO.getError_code()==0) {
+  System.out.println("My Float result="+myMO.getFloatResult());  
+    }
+ else System.out.println("Error"+myMO.getError_message());
+
 }
 
 
